155b313466cbcffa8bed80a3bd3b9889bfcdbcef
ydh6226 <ydh6226@naver.com>
2023-10-27 23:56:35 +0900

    도커파일 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
+
 plugins {
     id("org.springframework.boot") version "2.7.17"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
